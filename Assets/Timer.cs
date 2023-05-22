40b06e73176c9436a9b36f4ebb7f22ef5282d439
rifkigilangs <72380161+rifkigilangs@users.noreply.github.com>
2023-05-22 17:53:24 +0700

    Membuat Timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+   [SerializeField] TMP_Text TextTimer;
+   public float Waktu = 100;
+   public bool GameAktif = true;
+   public GameObject FinishedCanvas;
+
+   public void SetText()
+   {
+        int Menit = Mathf.FloorToInt(Waktu / 60);
+        int Detik = Mathf.FloorToInt(Waktu % 60);
+        TextTimer.text = Menit.ToString("00") + ":" + Detik.ToString("00");
+   }
+
+   float s;
+
+   private void Update() 
+   {
+        if(GameAktif)
+        {
+            s += Time.deltaTime;
+            if(s >= 1)
+            {
+                Waktu--;
+                s = 0;
+            }
+        }
+
+        if(GameAktif && Waktu <= 0)
+        {
+            Debug.Log("GameOver");
+            FinishedCanvas.SetActive(true);
+            GameAktif = false;
+        }
+
+
+        SetText();
+   }
+}
